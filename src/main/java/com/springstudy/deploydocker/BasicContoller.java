67dfcebda3af43324765b21165149ebd2bbc399e
ghkdwnsdlr <imbanana15@gmail.com>
2025-09-22 17:06:09 +0900

    scp-1

--- src/main/java/com/springstudy/deploydocker/BasicContoller.java
+++ src/main/java/com/springstudy/deploydocker/BasicContoller.java
@@ -1,4 +1,15 @@
 package com.springstudy.deploydocker;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class BasicContoller {
+
+    @ResponseBody
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
 }
